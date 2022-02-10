8f80a23bdbe1a42046404505011ad18925ca215c
Anders Heftøy Carlsen <anders.heftoy@gmail.com>
2022-02-10 15:21:38 +0100

    Added a Feedback view and a BaseInput.vue component

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  <h1>HEISANN</h1>
+</div>
+</template>
+
+<script>
+export default {
+  name: "FeedbackComponent"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Feedback.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="Feedback">
+    <h1>Please leave feedback</h1>
+  </div>
+  <base-input></base-input>
+</template>
+
+<script>
+// @ is an alias to /src
+import BaseInput from "@/components/BaseInput";
+
+export default {
+  name: "Feedback",
+  components: {
+    BaseInput,
+  },
+};
+</script>
+
